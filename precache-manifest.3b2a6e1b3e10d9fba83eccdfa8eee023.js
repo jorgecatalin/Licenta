75cff6406cfb433c9a777acf37df417b5218d13e
jorgecatalin <jorge_catalin@yahoo.com>
2019-11-23 21:15:44 +0200

    Updates

---
+++ precache-manifest.3b2a6e1b3e10d9fba83eccdfa8eee023.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bff90bc429a5cda202ec8cc7da15c2d2",
+    "url": "/licenta/index.html"
+  },
+  {
+    "revision": "d45b6440a3ba8c693358",
+    "url": "/licenta/static/css/main.60856d9a.chunk.css"
+  },
+  {
+    "revision": "3e2eabf59ccbb912de91",
+    "url": "/licenta/static/js/2.e671fc65.chunk.js"
+  },
+  {
+    "revision": "d45b6440a3ba8c693358",
+    "url": "/licenta/static/js/main.3e16ab4a.chunk.js"
+  },
+  {
+    "revision": "e8c3ad6fef03e0ab41c2",
+    "url": "/licenta/static/js/runtime-main.3f4fa536.js"
+  }
+]);
