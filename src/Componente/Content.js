284018091656d7eb2fa0677b1d5f96f855935925
jorgecatalin <jorge_catalin@yahoo.com>
2019-11-23 22:58:21 +0200

    incercare css

--- src/Componente/Content.js
+++ src/Componente/Content.js
@@ -7,7 +7,7 @@ import Intrebari from "./Intrebari.js"
 
 let intrebari = []
 
-intrebari.push(new Intrebari("Prima22 Intrebare", ["Da", "Nu"], [true, false]))
+intrebari.push(new Intrebari("Prima Intrebare", ["Da", "Nu"], [true, false]))
 
 class Content extends React.Component {
   constructor(props) {
