b3045011e51d23ac17253aa783394a271e85dc93
jorgecatalin <jorge_catalin@yahoo.com>
2019-12-18 12:14:14 +0200

    Updates

--- precache-manifest.770d2963afaf4e075f59e928f3554faa.js
+++ precache-manifest.770d2963afaf4e075f59e928f3554faa.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "cd87fafbc6e25d7a96595115fe703042",
+    "revision": "fb338a77beaacd5f848a20a18e44722e",
     "url": "/licenta/index.html"
   },
   {
-    "revision": "08224aeedb67b1422567",
-    "url": "/licenta/static/css/main.ad51c1fc.chunk.css"
+    "revision": "ae3bd25777dc7c27a8a7",
+    "url": "/licenta/static/css/main.4d1f8cbc.chunk.css"
   },
   {
     "revision": "04617709fae6f4136905",
     "url": "/licenta/static/js/2.7a03dcff.chunk.js"
   },
   {
-    "revision": "08224aeedb67b1422567",
-    "url": "/licenta/static/js/main.533d33f6.chunk.js"
+    "revision": "ae3bd25777dc7c27a8a7",
+    "url": "/licenta/static/js/main.5888bffc.chunk.js"
   },
   {
     "revision": "e8c3ad6fef03e0ab41c2",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/licenta/precache-manifest.5606c5aacb8aa8ea5b6bf0d49ffe77b1.js"
+  "/licenta/precache-manifest.770d2963afaf4e075f59e928f3554faa.js"
 );
 
 self.addEventListener('message', (event) => {
